e9f6bac83f8f78b6a2e2a7319a37012d5382ad1f
Sambath s <sivasambath16@gmail.com>
2025-10-06 21:24:29 +0530

    Delete backend - Copy directory

--- backend - Copy/models/Category.js
+++
@@ -1,9 +0,0 @@
-import mongoose from "mongoose";
-
-const categorySchema = new mongoose.Schema({
-  name: { type: String, required: true, unique: true },
-  description: String,
-  image: String,
-}, { timestamps: true });
-
-export default mongoose.model("Category", categorySchema);
